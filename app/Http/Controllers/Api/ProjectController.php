1778bed00aff5010a051b998c11a25852ee847f4
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-04-11 17:03:54 +0200

    updated api route and added show in api controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,22 @@ class ProjectController extends Controller
             'projects' => $projects
         ]);
     }
+
+    public function show($slug){
+        $project = Project::with('type', 'technology')->where('slug', $slug)->first();
+    
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        }
+        else {
+            return response()->json([
+                'success' => false,
+                'error' => 'No projects found'
+            ]);
+        }
+    }
 }
+
